95ed01f4f33a224b276da421057e8422732a3b70
Rafiklatrikdu69 <rafik.bouchenna050@email.com>
2024-02-10 09:46:39 +0100

    reglage secu app

--- AppKotlin/app/src/main/java/com/bouchenna/rafik/MainActivity.kt
+++ AppKotlin/app/src/main/java/com/bouchenna/rafik/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.bouchenna.rafik
-
-import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
+import android.widget.EditText
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
@@ -16,7 +12,8 @@ import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
-
+import java.util.Calendar
+import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,65 +22,74 @@ class MainActivity : AppCompatActivity() {
     private lateinit var textPrenom: TextView
     private lateinit var radioGroup: RadioGroup
     private lateinit var radioButton: RadioButton
-    private lateinit var textDate: TextView
+    private lateinit var textDate: EditText
     private lateinit var textTel: TextView
     private lateinit var textEmail: TextView
     private lateinit var checkBoxFavori: CheckBox
-
+    private var date: String? = ""
+    private var  EmailValidator:Email?=null
+    lateinit var datePicker: DatePickerHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button = findViewById<Button>(R.id.button2)
-        button.setOnClickListener {
+        datePicker = DatePickerHelper(this,true)
 
+        textDate = findViewById(R.id.date)
+
+
+        textDate.setOnClickListener {
+            showDatePickerDialog()
+        }
+
+        button = findViewById(R.id.button2)
+        button.setOnClickListener {
             textNom = findViewById(R.id.nom)
             textPrenom = findViewById(R.id.prenom)
-            textDate = findViewById(R.id.date)
             textEmail = findViewById(R.id.email)
             textTel = findViewById(R.id.telephone)
-            radioGroup = findViewById(R.id.groupe);
+            radioGroup = findViewById(R.id.groupe)
             checkBoxFavori = findViewById(R.id.checkBox)
-            var selected = radioGroup.getCheckedRadioButtonId()
-            var errorNom = textNom.text.length == 0;
-            var errorPrenom = textPrenom.text.length == 0;
-            var errorDate = textDate.text.length == 0;
-            var errorTel = textTel.text.length == 0;
-            var errorEmail = textEmail.text.length == 0;
-            var str1 = if (errorTel) "Aucune" else textTel.text.toString()//erreur a regler
-            var str2 = if (errorDate) "Aucune" else textDate.text.toString()
-            var checked = if (checkBoxFavori.isChecked()) "Ajouter au favori " else "Non"
+
+            val selected = radioGroup.checkedRadioButtonId
+            val errorNom = textNom.text.isEmpty()
+            val errorPrenom = textPrenom.text.isEmpty()
+            val errorDate = date?.isEmpty()
+            val errorTel = textTel.text.isEmpty()
+            val errorEmail = textEmail.text.isEmpty()
+
+            val str1 = if (errorTel) "Aucune" else textTel.text.toString()
+            val str2 = if (errorDate == true) "Aucune" else date
+
+            val checked = if (checkBoxFavori.isChecked) "Ajouter au favori " else "Non"
+
             val selectRadioButton = if (selected > 0) {
-                val radioButton = findViewById<View>(selected)
-                if (radioButton is TextView) {
-                    radioButton.text.toString()
-                } else {
-                    "Aucun"
-                }
+                val radioButton = findViewById<RadioButton>(selected)
+                radioButton.text.toString()
             } else {
                 "Aucun"
             }
 
-            val snack =
-                Snackbar.make(it, "Veuillez remplir les champs obligatoires*", Snackbar.LENGTH_LONG)
+            var snack =
+                Snackbar.make(it, "", Snackbar.LENGTH_LONG)
 
-            if (errorNom) {
-                textNom.error = "Remplir le champ nom ";
-            }
-            if (errorPrenom) {
-                textPrenom.error = "Remplir le champ prenom ";
-            }
-            if (errorEmail) {
-                textEmail.error = "Remplir le champ email ";
-            }
-            if (errorNom || errorPrenom || errorEmail) {
+            if (errorNom) textNom.error = "Remplir le champ nom"
+            if (errorPrenom) textPrenom.error = "Remplir le champ prenom"
+            if (errorEmail) textEmail.error = "Remplir le champ email"
+            if(!checkEmail(textEmail.text.toString())) {
+                textEmail.error = "Email Invalide"
+                snack =  Snackbar.make(it, "Email Invalide !", Snackbar.LENGTH_LONG)
                 snack.show()
-            } else {
-                val text = "Contact Créer"
-                val duration = Toast.LENGTH_SHORT
+            }
+            if (errorNom || errorPrenom || errorEmail ) {
+                snack=   Snackbar.make(it, "Veuillez remplir les champs obligatoires*", Snackbar.LENGTH_LONG)
 
+                snack.show()
+            } else if(checkEmail(textEmail.text.toString())){
+                val text = "Informations client"
+                val duration = Toast.LENGTH_SHORT
                 val toast = Toast.makeText(this, text, duration)
                 toast.show()
-                val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+                val builder = AlertDialog.Builder(this)
                 builder
                     .setMessage("Contact")
                     .setTitle("Informations du Contact")
@@ -91,8 +97,8 @@ class MainActivity : AppCompatActivity() {
                         ("nom : ${textNom.text.toString()}\n" +
                                 "Prénom : ${textPrenom.text.toString()}\n" +
                                 "Sexe : $selectRadioButton\n" +
-                                "téléphone : ${str1}\n" +
-                                "Date : ${str2}\n" +
+                                "téléphone : $str1\n" +
+                                "Date : $str2\n" +
                                 "Email : ${textEmail.text.toString()}\n"
                                 + "Favori : $checked")
                     )
@@ -101,7 +107,7 @@ class MainActivity : AppCompatActivity() {
                         val toast = Toast.makeText(this, text, duration)
                         toast.show()
                     }
-                val dialog: AlertDialog = builder.create()
+                val dialog = builder.create()
                 dialog.show()
             }
             Log.d("MainActivity", "click !")
@@ -109,8 +115,35 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onStart() {
-        super.onStart();
+        super.onStart()
         Log.d("MainActivity", "onStart")
     }
 
+    private fun showDatePickerDialog() {
+        val cal = Calendar.getInstance()
+        val d = cal.get(Calendar.DAY_OF_MONTH)
+        val m = cal.get(Calendar.MONTH)
+        val y = cal.get(Calendar.YEAR)
+        datePicker.showDialog(d, m, y, object : DatePickerHelper.Callback {
+            override fun onDateSelected(dayofMonth: Int, month: Int, year: Int) {
+                val dayStr = if (dayofMonth < 10) "0${dayofMonth}" else "${dayofMonth}"
+                val mon = month + 1
+                val monthStr = if (mon < 10) "0${mon}" else "${mon}"
+                date = "${dayStr}-${monthStr}-${year}"
+                textDate.setText(date, TextView.BufferType.EDITABLE);
+            }
+        })
+    }
+    val EMAIL_ADDRESS_PATTERN: Pattern = Pattern.compile(
+        "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                "\\@" +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                "(" +
+                "\\." +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                ")+"
+    )
+    private fun checkEmail(email: String): Boolean {
+        return EMAIL_ADDRESS_PATTERN.matcher(email).matches()
+    }
 }
